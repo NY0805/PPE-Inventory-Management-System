0ee215e52ad26d92ba6042b9de03cf4b055fd33c
Ng Yvonne <88yvonne.ng@gmail.com>
2025-02-13 21:08:59 +0800

    testing

---
+++ src/PPE_Inventory_Management_System/User.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PPE_Inventory_Management_System;
+
+/**
+ *
+ * @author user
+ */
+public class User {
+    public static void main (String[] args) {
+        
+    }
+}
